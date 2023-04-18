7c57acc6007bc44d0faca4f1655421d2b39078df
cy <darcyz@163.com>
2023-04-18 16:40:30 +0800

    cy

--- service/target/classes/com/example/controller/test/LocalController.java
+++ service/target/classes/com/example/controller/test/LocalController.java
@@ -3,6 +3,7 @@ package com.example.controller.test;
 import com.alibaba.fastjson.JSON;
 import com.example.entity.test.TestEntity;
 import com.example.service.test.TestService;
+import com.example.utils.AjaxResult;
 import com.example.utils.SendRequest;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -30,41 +32,53 @@ public class LocalController {
     }
 
     @RequestMapping(value = "/list",method = RequestMethod.GET)
-    public PageInfo<TestEntity> list(@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
-                                     @RequestParam(value = "pageSize",defaultValue = "5") int pageSize){
-        PageHelper.offsetPage((pageNum-1) * pageSize,pageSize);
+    public AjaxResult list(@RequestParam(value = "page",defaultValue = "1") int page,
+                           @RequestParam(value = "limit",defaultValue = "10") int limit){
+        AjaxResult ajaxResult = new AjaxResult();
+        HashMap map = new HashMap();
+        PageHelper.offsetPage((page-1) * limit,limit);
         List<TestEntity> list = testService.getList();
         logger.info("list:"+ JSON.toJSONString(list));
         PageInfo<TestEntity> pageInfo = new PageInfo<>(list);
         //计算总页数
-        int pageNumTotal = (int) Math.ceil((double)pageInfo.getTotal()/(double)pageSize);
-        if(pageNum > pageNumTotal){
+        int pageNumTotal = (int) Math.ceil((double)pageInfo.getTotal()/(double)limit);
+        if(page > pageNumTotal){
             PageInfo<TestEntity> entityPageInfo = new PageInfo<>();
             entityPageInfo.setList(new ArrayList<>());
             entityPageInfo.setTotal(pageInfo.getTotal());
-            entityPageInfo.setPageNum(pageNum);
-            entityPageInfo.setPageSize(pageSize);
-            return entityPageInfo;
+            entityPageInfo.setPageNum(page);
+            entityPageInfo.setPageSize(limit);
+            map.put("pageInfo",entityPageInfo);
         }else {
-            return pageInfo;
+            map.put("pageInfo",pageInfo);
         }
+        ajaxResult.setCode(20000);
+        ajaxResult.setMessage("成功");
+        ajaxResult.setData(map);
+        logger.info("responseMsg:"+ JSON.toJSONString(ajaxResult));
+        return ajaxResult;
     }
 
     @RequestMapping(value = "/save",method = RequestMethod.POST)
-    public String save(TestEntity testEntity){
+    public AjaxResult save(@RequestBody TestEntity testEntity){
+        AjaxResult ajaxResult = new AjaxResult();
         if(testEntity.getId() != null){
             testService.update(testEntity);
-            return "修改成功";
         }else{
             testService.save(testEntity);
-            return "保存成功";
         }
+        ajaxResult.setCode(20000);
+        ajaxResult.setMessage("成功");
+        return ajaxResult;
     }
 
     @RequestMapping(value = "/delete",method = RequestMethod.GET)
-    public String delete(@RequestParam(value = "id") int id){
+    public AjaxResult delete(@RequestParam(required=false, value = "id") Integer id){
+        AjaxResult ajaxResult = new AjaxResult();
         testService.delete(id);
-        return "删除成功";
+        ajaxResult.setCode(20000);
+        ajaxResult.setMessage("成功");
+        return ajaxResult;
     }
 
     @RequestMapping(value = "/sendRequest",method = RequestMethod.GET)
